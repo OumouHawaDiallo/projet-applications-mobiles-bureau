c50e88850fa49c38aaf322b44dc86279d26fd844
oumou <oumouhawadiallo@esp.sn>
2024-01-12 18:38:53 +0000

    premier  commit

---
+++ MyMauiApp1/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MyMauiApp1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MyMauiApp1/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MyMauiApp1
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
