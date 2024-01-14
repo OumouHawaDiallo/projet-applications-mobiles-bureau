54468f3ebb24089b86e9f0b7e301a542b0327bda
oumou <oumouhawadiallo@esp.sn>
2024-01-14 16:48:37 +0000

    deuxieme commit

---
+++ Phoneword/Phoneword/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Phoneword
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
+
