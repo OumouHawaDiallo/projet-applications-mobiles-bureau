cc819bcd5487d24b36f691e985a4b62c3f53cb8f
oumou <oumouhawadiallo@esp.sn>
2024-01-16 02:31:45 +0000

    6-exercise-create-phone-number-translator-app

--- Phoneword/Phoneword/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Phoneword.AssemblyInfo.cs
+++ Phoneword/Phoneword/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Phoneword.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Phoneword")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+c50e88850fa49c38aaf322b44dc86279d26fd844")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Phoneword")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Phoneword")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
